a11aab57e7526fab42161f82772a194dd84a2ad6
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-05-10 16:39:22 -0300

    feat: configure user context with Content API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,30 +2,39 @@ import { Button } from '@material-ui/core';
 import { Container, Titulo, InputContainer } from './styles';
 import { Input, InputLabel, InputAdornment } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { UserContext } from 'common/context/User';
 
 function Login() {
   const history = useHistory();
   return (
     <Container>
-      <Titulo>Insira o seu nome</Titulo>
-      <InputContainer>
-        <InputLabel>Nome</InputLabel>
-        <Input type="text" />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Saldo</InputLabel>
-        <Input
-          type="number"
-          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
-        />
-      </InputContainer>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => history.push('/feira')}
-      >
-        Avançar
-      </Button>
+      <UserContext.Consumer>
+        {({ name, setName, balance, setBalance }) => (
+          <>
+            <Titulo>Insira o seu nome</Titulo>
+            <InputContainer>
+              <InputLabel>Nome</InputLabel>
+              <Input type="text" />
+            </InputContainer>
+            <InputContainer>
+              <InputLabel>Saldo</InputLabel>
+              <Input
+                type="number"
+                startAdornment={
+                  <InputAdornment position="start">R$</InputAdornment>
+                }
+              />
+            </InputContainer>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => history.push('/feira')}
+            >
+              Avançar
+            </Button>
+          </>
+        )}
+      </UserContext.Consumer>
     </Container>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,26 @@ import Feira from 'pages/Feira';
 import Login from 'pages/Login';
 import { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { UserContext } from 'common/context/User';
 
 const Router = () => {
+  const [name, setName] = useState('');
+  const [balance, setBalance] = useState(0);
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <Login />
+          <UserContext.Provider value={{ name, setName, balance, setBalance }}>
+            <Login />
+          </UserContext.Provider>
         </Route>
         <Route path="/feira">
-          <Feira />
+          <Feira
+            name={name}
+            setName={setName}
+            balance={balance}
+            setBalance={setBalance}
+          />
         </Route>
         <Route path="/carrinho">
           <Carrinho />
